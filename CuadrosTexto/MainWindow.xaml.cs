7b0be567e65d0659b5612339b3220c5ae44e9cb9
Marga <71320648+margabalmis@users.noreply.github.com>
2021-10-01 21:49:24 +0200

    Agregar archivos de proyecto.

---
+++ CuadrosTexto/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CuadrosTexto
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void fTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.F1)
+            {
+                TextBox textoAComprobar = sender as TextBox;
+
+                if (textoAComprobar == nombreTextBox)
+                    ayudaNombreTextBlock.Text = textoAComprobar.Tag.ToString();
+                else
+                    ayudaApellidoTextBlock.Text = textoAComprobar.Tag.ToString();
+                 
+            }
+            if (e.Key == Key.F2)
+            {
+               
+                validarEdadTextBlock.Text = Int32.TryParse(edadTextBox.Text.ToString(), out _) ? "" : "Edad incorrecta";
+            }
+
+        }
+    }
+}
